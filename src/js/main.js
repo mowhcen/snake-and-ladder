4f7acbd9cb6ba4e9d67ea74528f7832299444a2a
Mohammad Mohsen Hosseini <mowhcen@gmail.com>
2021-09-14 21:31:17 +0430

    feat: create board cells with js DOM

--- src/js/main.js
+++ src/js/main.js
@@ -4,12 +4,12 @@ let zero = "";
 for (let countRow = 0; countRow < 5; countRow++) {
     for (let count = 0; count < 5; count++) {
         $(".board").append(
-            `<div class="odd-cel"><span class="cel-number">${
+            `<div class="odd-cel"><span class="odd-cel__number">${
                 100 - countRow * 20 - count * 2
             }</span></div>`
         );
         $(".board").append(
-            `<div class="even-cel"><span class="cel-number">${
+            `<div class="even-cel"><span class="even-cel__number">${
                 99 - countRow * 20 - count * 2
             }</span></div>`
         );
@@ -17,13 +17,13 @@ for (let countRow = 0; countRow < 5; countRow++) {
     for (let count = 0; count < 5; count++) {
         countRow === 4 ? (zero = "0") : (zero = "");
         $(".board").append(
-            `<div class="even-cel"><span class="cel-number">${zero}${
+            `<div class="even-cel"><span class="even-cel__number">${zero}${
                 81 - countRow * 20 + count * 2
             }</span></div>`
         );
         countRow === 4 && count !== 4 ? (zero = "0") : (zero = "");
         $(".board").append(
-            `<div class="odd-cel"><span class="cel-number">${zero}${
+            `<div class="odd-cel"><span class="odd-cel__number">${zero}${
                 82 - countRow * 20 + count * 2
             }</span></div>`
         );
