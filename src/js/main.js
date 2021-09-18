1d1396f4752a278fe88a1099325987bd3aaff505
Mohammad Mohsen Hosseini <mowhcen@gmail.com>
2021-09-18 15:17:35 +0430

    refactor: structure file a litle bit

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,7 @@ import { allCell, initialCellBoard, positionCelNumbers } from "./board";
 
 import initialInput from "./input";
 import roleDice from "./dice";
+import sampleMove from "./move";
 
 initialCellBoard();
 
@@ -15,26 +16,4 @@ initialInput();
 
 roleDice();
 
-$("#blue").appendTo(".cel-3");
-
-$("#blue").addClass("end-point");
-
-$("#blue").addClass("ladder-clime-six");
-
-$("#yellow").appendTo(".cel-6");
-
-$("#yellow").addClass("end-point");
-
-$("#yellow").addClass("move-three-r");
-
-$("#green").appendTo(".cel-76");
-
-$("#green").addClass("end-point");
-
-$("#green").addClass("eaten-by-snake-fourth");
-
-$("#red").appendTo(".cel-95");
-
-$("#red").addClass("end-point");
-
-$("#red").addClass("move-five-l");
+sampleMove();
--- src/js/move.js
+++ src/js/move.js
@@ -1,1 +1,29 @@
 "use strick";
+
+const sampleMove = () => {
+    $("#blue").appendTo(".cel-3");
+
+    $("#blue").addClass("end-point");
+
+    $("#blue").addClass("ladder-clime-six");
+
+    $("#yellow").appendTo(".cel-6");
+
+    $("#yellow").addClass("end-point");
+
+    $("#yellow").addClass("move-three-r");
+
+    $("#green").appendTo(".cel-76");
+
+    $("#green").addClass("end-point");
+
+    $("#green").addClass("eaten-by-snake-fourth");
+
+    $("#red").appendTo(".cel-95");
+
+    $("#red").addClass("end-point");
+
+    $("#red").addClass("move-five-l");
+};
+
+export { sampleMove as default };
