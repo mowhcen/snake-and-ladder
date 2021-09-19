6253a60bee9e035b8084a5d4d27f7b3ddb8341cc
Mohammad Mohsen Hosseini <mowhcen@gmail.com>
2021-09-19 23:49:59 +0430

    refactor: rm usless animation code and classes

--- src/js/move.js
+++ src/js/move.js
@@ -5,25 +5,17 @@ const sampleMove = () => {
 
     $("#blue").addClass("end-point");
 
-    $("#blue").addClass("ladder-clime-six");
-
     $("#yellow").appendTo("#cel-6");
 
     $("#yellow").addClass("end-point");
-
-    $("#yellow").addClass("move-three-r");
 
     $("#green").appendTo("#cel-76");
 
     $("#green").addClass("end-point");
 
-    $("#green").addClass("eaten-by-snake-fourth");
-
     $("#red").appendTo("#cel-95");
 
     $("#red").addClass("end-point");
-
-    $("#red").addClass("move-five-l");
 };
 
 export { sampleMove as default };
