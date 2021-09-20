7603bb4ffdb94df2b4b025e3c1a60c4dc273f67b
Mohammad Mohsen Hosseini <mowhcen@gmail.com>
2021-09-20 18:17:26 +0430

    refactor: change place of file that some function call

--- src/js/main.js
+++ src/js/main.js
@@ -16,8 +16,6 @@ import sampleMove from "./move";
 
 initialCellBoard();
 
-positionCelNumbers(allCell());
-
 initialInput();
 
 roleDice();
@@ -27,18 +25,3 @@ sampleMove();
 dropDown();
 
 choseColor();
-
-drawLine(3, 60, "left-ladder");
-drawLine(6, 27, "right-ladder");
-drawLine(11, 70, "vertical");
-drawLine(35, 56, "left-ladder");
-drawLine(68, 93, "vertical");
-drawLine(64, 99, "left-ladder");
-
-drawLine(37, 1, "right-snake");
-drawLine(25, 5, "vertical");
-drawLine(47, 9, "left-snake");
-drawLine(65, 59, "right-snake");
-drawLine(87, 54, "vertical");
-drawLine(96, 61, "right-snake");
-drawLine(92, 69, "vertical");
--- src/js/move.js
+++ src/js/move.js
@@ -1,20 +1,13 @@
 "use strick";
 
 const sampleMove = () => {
-    $("#blue").appendTo("#cel-3");
-
+    $("#blue").appendTo("#cel-1");
     $("#blue").addClass("end-point");
-
-    $("#yellow").appendTo("#cel-6");
-
+    $("#yellow").appendTo("#cel-1");
     $("#yellow").addClass("end-point");
-
-    $("#green").appendTo("#cel-76");
-
+    $("#green").appendTo("#cel-1");
     $("#green").addClass("end-point");
-
-    $("#red").appendTo("#cel-95");
-
+    $("#red").appendTo("#cel-1");
     $("#red").addClass("end-point");
 };
 
